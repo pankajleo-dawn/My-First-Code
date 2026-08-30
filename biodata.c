fca766a886c00bc187f05753791441fc9986b9a0
Pankaj Naik <pankaj.naiklm10@gmail.com>
2026-08-30 10:34:25 +0530

    added biodata program

---
+++ biodata.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    printf("My name is Pankaj Naik\n");
+    printf("I m from Jampani, Nuapada, Odisha\n");
+    printf("I passed my 12th class from Lzebra Public School, in Kota Rajasthan\n");
+    printf("My hobbies are playing football and badminton, and watching animes");
+}
