733abaa26c32d5034884b8c9fedbd56b45831ab8
Pankaj _ VSSUT <pankaj.naiklm10@gmail.com>
2026-08-30 12:05:23 +0530

    added arithemetic program

---
+++ arithmeticOperation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int num1, num2, sum;
+    printf("num1:");
+    scanf("%d", &num1);
+    printf("\nnum2:");
+    scanf("%d", &num2);
+    sum = num1 + num2 ;
+    printf("sum=%d", sum);
+    return 0;
+}
