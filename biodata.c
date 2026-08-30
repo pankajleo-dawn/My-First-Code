3d1ccb5889fb6d823658f7fb8098ffca36563b87
Pankaj _ VSSUT <pankaj.naiklm10@gmail.com>
2026-08-30 11:35:58 +0530

    Added variable program

--- biodata.c
+++ biodata.c
@@ -4,4 +4,5 @@ int main () {
     printf("I m from Jampani, Nuapada, Odisha\n");
     printf("I passed my 12th class from Lzebra Public School, in Kota Rajasthan\n");
     printf("My hobbies are playing football and badminton, and watching animes");
+    return 0;
 }
---
+++ variable.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    int a,b;
+    printf("enter two number: ");
+    scanf("%d", &a);
+    scanf("%d", &b);
+    printf("a=%d,b=%d,",a,b);
+    return 0;
+}
