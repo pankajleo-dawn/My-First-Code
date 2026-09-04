6870f6d746dcf92a8b1b73582a51719ec786415b
Pankaj _ VSSUT <pankaj.naiklm10@gmail.com>
2026-09-04 16:23:37 +0530

    declared variable and use

---
+++ pankaj.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int num1;
+    int num2;
+    num1=10;
+    num2=20;
+    printf("pankaj=%d",num2);
+    return 0;
+}
---
+++ variablenum1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int num1;
+    int num2;
+    num1=10;
+    num2=20;
+    printf("num1=%d\n",num1);
+    printf("num2=%d",num2);
+    return 0;
+}
