085f506f1b14e03a01866568c3a32f379fc5ce72
Pankaj _ VSSUT <pankaj.naiklm10@gmail.com>
2026-09-19 00:59:11 +0530

    Relational_Operator

---
+++ relational_operator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b ;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if (a==b){
+        printf("yes equal");}
+    if (a>=b){
+        printf("i will not go to college");}
+    if (a<b){
+        printf("i will go to college");}
+     return 0;
+}
